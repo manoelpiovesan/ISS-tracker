26a4357575b5bee2d9b385b5f6a773f19553b8dd
Manoel <manoelpiovesan@gmail.com>
2022-09-07 18:35:55 -0300

    tracker pronto

--- script.js
+++ script.js
@@ -1,27 +1,74 @@
-var map = L.map('map').setView([-22.285353906676182,-42.539959456883565], 2);
+// var map = L.map('map').setView([-22.285353906676182,-42.539959456883565], 6 );
 
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png	', {
+
+
+// L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png	', {
+//     maxZoom: 19,
+// }).addTo(map);
+
+
+var cities = L.layerGroup();
+
+var mbAttr = '';
+var mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
+    attribution: ''
+});
+
+var dark = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: ''
+});
+
+var light = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: ''
+});
 
 
-document.querySelector('#get-iss-coords').addEventListener('click', ()=>{getISSCoords()})
 
-var ListaDeCoordenadas = []
+var map = L.map('map', {
+    center: [-22.285353906676182, -42.539959456883565],
+    zoom: 10,
+    layers: [dark, cities]
+});
+
+var baseLayers = {
+    'Dark': dark,
+    'Light': light,
+    'Default': osm,
+
+};
+
+var overlays = {
+   
+};
+
+var layerControl = L.control.layers(baseLayers, overlays).addTo(map);
+
+
+
+var satellite = L.tileLayer(mbUrl, {id: 'mapbox/satellite-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
+layerControl.addBaseLayer(satellite, 'Satellite');
+
+
 
 
 var inverval_timer;
 
 inverval_timer = setInterval(function() { 
     getISSCoords()
-}, 7000);
-    
-//IF you want to stop above timer
+}, 5000);
+
 function stop_timer() {
     clearInterval(inverval_timer); 
 }
 
+var ListaDeCoordenadas = []
 
 function getISSCoords(){
     const url = 'http://api.open-notify.org/iss-now.json'
@@ -35,16 +82,18 @@ function getISSCoords(){
         let latitude =  parseFloat(data.iss_position.latitude)
         let longitude = parseFloat(data.iss_position.longitude)
 
+        getCityName(latitude,longitude)
+
         // var marker = L.marker([latitude, longitude]).addTo(map);
 
         var circle = L.circle([latitude, longitude], {
-            color: 'red',
-            fillColor: '#f03',
+            color: '#BB86FC',
+            fillColor: '#BB86FC',
             fillOpacity: 0.5,
-            radius: 50
+            radius: 150
         }).addTo(map);
 
-        map.setView([latitude,longitude], 7);
+        map.setView([latitude,longitude], map.getZoom());
 
         var atualCoordenada = new L.LatLng(latitude, longitude);
        
@@ -60,9 +109,9 @@ function getISSCoords(){
 
         
         var firstpolyline = new L.Polyline(ListaDeCoordenadas, {
-            color: 'red',
+            color: '#BB86FC',
             weight: 3,
-            opacity: 0.4,
+            opacity: 0.7,
             smoothFactor: 1
         });
         firstpolyline.addTo(map);
@@ -73,3 +122,34 @@ function getISSCoords(){
     console.log(error);
     });
 }
+
+function getCityName(latitude, longitude){
+
+    const URL = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=pt`
+    let locationData;
+    
+    fetch(URL)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        
+        console.log(data)
+        locationData = data
+
+        document.querySelector('#locality').textContent = data.locality 
+        
+       
+
+    })
+    .catch(function(error) {
+    console.log(error);
+    });
+
+    return locationData
+
+}
+
+
+
+
+
+ 
